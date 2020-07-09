7e7f43351c2e469a107da8e16f6108047340654d
Jesus Esquer <jme2791@gmail.com>
2020-07-08 19:59:31 -0700

    js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Hola mundo!!");
+const textArea = document.querySelector('[name="text"]');
+const result = document.querySelector(".result");
+const filterInputs = document.querySelectorAll('[name="filter"]');
+
+function transformText(text) {
+    //take the text, and loop over each letter
+    const mod = Array.from(text);
+    console.log(mod)
+  result.textContent = text;
+}
+
+const filters = {
+  sarcastic() {},
+  funky() {},
+  unable() {},
+};
+
+textArea.addEventListener("input", (e) => transformText(e.target.value));
